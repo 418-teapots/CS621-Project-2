d32badca011c6405a6178d8791e6151d788e8560
Tae Lee <leeth7830@gmail.com>
2019-04-28 11:46:41 -0700

    Update cs621-dev02.cc

--- ns-3.29/examples/routing/cs621-dev02.cc
+++ ns-3.29/examples/routing/cs621-dev02.cc
@@ -173,7 +173,7 @@ int main (int argc, char *argv[])
     Ptr<NetDevice> netDevice = d1d2.Get(0);
     Ptr<PointToPointNetDevice> p2pNetDevice = StaticCast<PointToPointNetDevice>(netDevice);
     Ptr<DRR> drrQueue = Create<DRR>();
-    p2pNetDevice.SetQueue(drrQueue);
+    p2pNetDevice->SetQueue(drrQueue);
     // (Client)
     // Create a RequestResponseClient application to send UDP datagrams from node zero to node three.
     Time interPacketInterval = Seconds (0.01);
